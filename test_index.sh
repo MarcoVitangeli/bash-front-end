d596e37cfb7de538a3a9df22c59c3c597d8e00f5
MarcoVitangeli <marco_augusto_vitangeli@outlook.com>
2023-09-08 04:09:05 -0300

    removed file

--- test_index.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-function urldecode() {
-    : "${*//+/ }"; echo -e "${_//%/\\x}"; 
-}
-
-x="http%3A%2F%2Fstackoverflow.com%2Fsearch%3Fq%3Durldecode%2Bbash"
-y=$(urldecode "$x")
-echo "$y"
